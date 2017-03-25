c28cab3fd1c965172cc30ae5bc2abab6397c4150
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2017-03-25 11:17:35 +0200

    tt

---
+++ PortfolioCMS/PortfolioCMS.Tests/PortfolioCMS.Business.Data.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PortfolioCMS.Business.Data.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestMejkthod1()
+        {
+            var test = true;
+
+            Assert.AreEqual(test, true);
+        }
+
+        [Test]
+        public void TestMasdasejkthod1()
+        {
+            var test = true;
+
+            Assert.AreEqual(test, true);
+        }
+    }
+}
